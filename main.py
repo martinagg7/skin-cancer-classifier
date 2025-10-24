7aaffa0cc1936de27ef88b5af802336dcd21785f
Martina <martinagg@myuax.com>
2025-10-24 14:37:44 +0200

    upload: final changes

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 from src.preprocessing.pipeline import procesar_carpeta
 
 if __name__ == "__main__":
-    # --- TRAIN ---
+    # --- EXTERNAL ---
     procesar_carpeta(
-        input_folder="data/images/train",
-        zoomed_folder="data/zoomed/train",
-        masks_folder="data/masks/train",
-        lesions_folder="data/lesions/train",
+        input_folder="data/external",
+        zoomed_folder="data/zoomed/external",
+        masks_folder="data/masks/external",
+        lesions_folder="data/lesions/external",
         zoom_factor=0.9,
         size=(224, 224)
     )
 
-    # --- TEST ---
-    procesar_carpeta(
-        input_folder="data/images/test",
-        zoomed_folder="data/zoomed/test",
-        masks_folder="data/masks/test",
-        lesions_folder="data/lesions/test",
-        zoom_factor=0.9,
-        size=(224, 224)
-    )
-
-    print("✅ Zoom, máscaras y métricas generadas para TRAIN y TEST.")
+    print("✅ Zoom, máscaras y métricas generadas para EXTERNAL.")
